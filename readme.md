ff9e2e146db6b68ce07013d4f834301a4ac907d3
devjanaprime <dev@primeacademy.io>
2016-08-26 14:46:12 -0500

    added basic input checks

--- readme.md
+++ readme.md
@@ -5,9 +5,12 @@ Vanilla Javascript project in-class to review variables, functions, objects, and
 
 Versions/Branches
 ------------
-* 0.1 - html/js handshake
-* 0.2 - basic user input
-* 0.3 - create car object from user input
-* 0.4 - push car object into "garage" array
-* 0.5 - list cars on DOM
-* 0.6 - display cars on DOM
+* 0.1 - html/js handshake [x]
+* 0.2 - basic user input [x]
+* 0.3 - create car object from user input [x]
+* 0.4 - push car object into "garage" array [x]
+* 0.5 - list cars on DOM [x]
+* 0.6 - display cars on DOM [x]
+* 0.7 - clear inputs [x]
+* 0.8 - check for empties [x]
+* 0.9 -
--- scripts/garage.js
+++ scripts/garage.js
@@ -4,8 +4,7 @@ var garage=[];
 
 var addCar = function(){
   console.log( 'in addCar' );
-  // get user input
-  // create car object
+  // create car object from user input
   var newCar = {
     year: document.getElementById( 'yearIn' ).value,
     make: document.getElementById( 'makeIn' ).value,
@@ -13,30 +12,41 @@ var addCar = function(){
     picURL: document.getElementById( 'picURLIn' ).value,
     description: document.getElementById( 'descriptionIn' ).value
   }; // end newCar
-  console.log( newCar );
-  // push car into garage
-  garage.push( newCar );
-  // display cars
-  displayCars();
+  // alert user if necessary fields are left blank
+  if( newCar.year == '' || newCar.make == '' || newCar.model == '' ){
+    alert( 'Please fill stuff in, yo' );
+  } // end missing inputs
+  else{
+    // continue if needed fields are filled in
+    clearInputs();
+    // push car into garage
+    garage.push( newCar );
+    // display cars
+    displayCars();
+  } // end all needed inputs filled is
 } // end addCar
+
+var clearInputs = function(){
+  console.log( 'in clearInputs' );
+  document.getElementById( 'yearIn' ).value='';
+  document.getElementById( 'makeIn' ).value='';
+  document.getElementById( 'modelIn' ).value='';
+  document.getElementById( 'picURLIn' ).value='';
+  document.getElementById( 'descriptionIn' ).value='';
+} // end clearInputs
 
 var displayCars = function(){
   console.log( 'in displayCars' );
   console.log( garage );
-  // empty our ul element
+  // empty our div element
   document.getElementById( 'allCars' ).innerHTML='';
   // for each car in the garage, add a list item with year, make, and modelIn
   for( var i = 0; i < garage.length; i++ ){
     // car information (year, make, model)
-    var carInfo = garage[ i ].year + ' ' + garage[ i ].make + ' ' + garage[ i ].model;
-    // put carInfo inside a list item through a textNode
-    // create the li
-    var listItem = document.createElement( 'li' );
-    // create the textNode
-    var textNode = document.createTextNode( carInfo );
-    // append textNode as child to li
-    listItem.appendChild( textNode );
-    // append list item to the ul
-    document.getElementById( 'allCars' ).appendChild( listItem );
+    var carInfo = '<h2>' + garage[ i ].year + ' ' + garage[ i ].make + ' '
+    + garage[ i ].model + '</h2><img src="' + garage[ i ].picURL + '"><p>'
+    + garage[ i ].description + '</p>';
+    // append car info to output div
+    document.getElementById( 'allCars' ).innerHTML += carInfo;
   } // end for
 } // end displayCars
